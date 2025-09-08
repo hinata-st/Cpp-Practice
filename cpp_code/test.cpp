88392261911d83f8cc7dbcb3eb3206883a8d4efe
Emilia-tan <1981859435@qq.com>
2025-09-08 10:48:18 +0800

    build cpp program

---
+++ cpp_code/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    cin >> num;
+    cout << "hello world" << endl;
+    cout << "hello git" << endl;
+    cout << "num = " << num << endl;
+    return 0;
+}
