697913e5886d8e78b9d270aede9316871d8b2ca2
Emilia-tan <1981859435@qq.com>
2025-11-13 19:34:10 +0800

    feat(offer): 复习offer.43

--- algorithm/leetcode/offer/12.cpp
+++ algorithm/leetcode/offer/12.cpp
@@ -323,3 +323,40 @@ class mySolution_12_3
         return false;
     }
 };
+
+class mySolution_12_4
+{
+    public:
+    bool exist(vector<vector<char>> &board, string word)
+    {
+        int n = board.size();
+        int m = board[0].size();
+        function<bool(int, int, int)> dfs = [&](int i, int j, int k) -> bool
+        {
+            if(k == word.size())
+            {
+                return true;
+            }
+            if(i < 0 || i >= n || j < 0 || j >= m || board[i][j] != word[k])
+            {
+                return false;
+            }
+            char temp = board[i][j];
+            board[i][j] = '.';
+            bool res = dfs(i - 1, j, k + 1) || dfs(i + 1, j, k + 1) || dfs(i, j - 1, k + 1) || dfs(i, j + 1, k + 1);
+            board[i][j] = temp;
+            return res;
+        };
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (dfs(i, j, 0))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
