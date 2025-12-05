7c893ec97ed91157a00355bd2814d35421579303
Emilia-tan <1981859435@qq.com>
2025-12-05 15:38:02 +0800

    feat(offer): 完成offer.60

---
+++ algorithm/leetcode/offer/60.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// question :
+
+class mySolution_60
+{
+    public:
+    vector<float> dicesProbability(int n)
+    {
+        vector<float> dp(5 * n + 1,0.0);
+        int total = pow(6,n);
+        dp[0] = 1.0 / total;
+        for (int i = 1; i < 6; ++i)
+        {
+            if (n == 1)
+            {
+                dp[i] = 1.0 / total;
+            }
+            else 
+            {
+                dp[i] = dp[i - 1] + 1.0 / total;
+            }
+        }
+        for (int i = 7; i < 5 * n + 1 - 5; ++i)
+        {
+            dp[i] = dp[i - 1];
+        }
+        for (int i = 5 * n + 1 - 5; i < 5 * n + 2 && n != 1; ++i)
+        {
+            dp[i] = dp[i - 1] - 1.0 / total;
+        }
+        return dp;
+    }
+};
+
+class offerSolution_60
+{
+public:
+    vector<double> dicesProbability(int n)
+    {
+        int f[n + 1][6 * n + 1];
+        memset(f, 0, sizeof f);
+        for (int j = 1; j <= 6; ++j)
+        {
+            f[1][j] = 1;
+        }
+        for (int i = 2; i <= n; ++i)
+        {
+            for (int j = i; j <= 6 * i; ++j)
+            {
+                for (int k = 1; k <= 6; ++k)
+                {
+                    if (j >= k)
+                    {
+                        f[i][j] += f[i - 1][j - k];
+                    }
+                }
+            }
+        }
+        vector<double> ans;
+        double m = pow(6, n);
+        for (int j = n; j <= 6 * n; ++j)
+        {
+            ans.push_back(f[n][j] / m);
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    mySolution_60 solution;
+    int n = 2;
+    vector<float> res = solution.dicesProbability(n);
+    cout << "The dicesProbability result is : " << endl;
+    for (float num : res)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
