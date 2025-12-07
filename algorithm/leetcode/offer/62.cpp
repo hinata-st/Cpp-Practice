26c6972cd87d1ce014c3a5fdb614f48582a2f0a6
Emilia-tan <1981859435@qq.com>
2025-12-07 21:34:32 +0800

    feat(offer): 完成offer.62

---
+++ algorithm/leetcode/offer/62.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// question : 0,1,···,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字（删除后从下一个数字开始计数）。求出这个圆圈里剩下的最后一个数字。例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+
+class mySolution_62
+{
+    public:
+    int lastRemaining(int n, int m)
+    {
+        int rest = n;
+        int dex = 0;
+        int i = 0;
+        unordered_set<int> st;
+        while(rest > 1)
+        {        
+            if (i == m - 1)
+            {
+                st.insert(dex);
+                dex = dex + 1;
+                if (dex >= n)
+                {
+                    dex = 0;
+                }
+                while (st.find(dex) != st.end())
+                {
+                    dex++;
+                    if (dex >= n)
+                    {
+                        dex = 0;
+                    }
+                }
+                i = 0;
+                rest--;
+            }
+            else
+            {
+                dex++;
+                if (dex >= n)
+                {
+                    dex = 0;
+                }
+                while(st.find(dex) != st.end())
+                {
+                    dex++;
+                    if (dex >= n)
+                    {
+                        dex = 0;
+                    }
+                }
+                i++;
+            }
+        }
+        for (int j = 0; j < n; j++)
+        {
+            if (st.find(j) == st.end())
+            {
+                return j;
+            }
+        }
+        return -1;
+    }
+};
+
+class offerSolution_62
+{
+public:
+    int lastRemaining(int n, int m)
+    {
+        int f = 0;
+        for (int i = 2; i <= n; ++i)
+        {
+            f = (f + m) % i;
+        }
+        return f;
+    }
+};
+
