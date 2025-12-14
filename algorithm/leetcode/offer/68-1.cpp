4f192941e59ffb364f57135c2e6e0aed322b7c19
Emilia-tan <1981859435@qq.com>
2025-12-14 19:35:03 +0800

    feat(offer): 完成offer.68-1

---
+++ algorithm/leetcode/offer/68-1.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+// question : 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+    class mySolution_68_1
+{
+    public:
+        TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+        {
+            if (root == nullptr || root == p || root == q)
+            {
+                return root;
+            }
+            if (p->val < root->val && q->val < root->val)
+            {
+                return lowestCommonAncestor(root->left, p, q);
+            }
+            else if (p->val > root->val && q->val > root->val)
+            {
+                return lowestCommonAncestor(root->right, p, q);
+            }
+            else
+            {
+                return root;
+            }
+        }
+};
+
+class offerSolution_68_1
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root->val < p->val && root->val < q->val)
+        {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        if (root->val > p->val && root->val > q->val)
+        {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        return root;
+    }
+};
